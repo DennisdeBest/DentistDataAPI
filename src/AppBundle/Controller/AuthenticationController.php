5e2f9b93ff4060f630c82f7b8f3c6e24affc69f7
DennisdeBest <dennis@debest.fr>
2017-01-16 12:27:42 +0100

    Serialize api response

--- src/AppBundle/Controller/AuthenticationController.php
+++ src/AppBundle/Controller/AuthenticationController.php
@@ -3,6 +3,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Security\TokenAuthenticator;
 use FOS\UserBundle\Controller\RegistrationController as BaseController;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\ExpiredTokenException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -23,13 +24,18 @@ class AuthenticationController extends BaseController
         $authenticator = $this->get('token_authenticator');
         $token = $authenticator->getCredentials($request);
         if($token){
-            $data = $authenticator->getUser($token, $this->get('fos_user.user_manager'));
+            try{
+                $data = $authenticator->getUser($token, $this->get('fos_user.user_manager'));
+            } catch (ExpiredTokenException $e){
+                $response = new Response($this->serialize("Token expired"), Response::HTTP_FORBIDDEN);
+                return $this->setBaseHeaders($response);
+            }
         } else {
-            $response = new Response("Bad crendentials", Response::HTTP_FORBIDDEN);
+            $response = new Response($this->serialize("Missing token"), Response::HTTP_FORBIDDEN);
             return $this->setBaseHeaders($response);
         }
         if(!$data){
-            $response = new Response("Bad crendentials", Response::HTTP_FORBIDDEN);
+            $response = new Response($this->serialize("Bad credentials"), Response::HTTP_FORBIDDEN);
         }else {
             $response = new Response($this->serialize([$data]), Response::HTTP_OK);
         }
