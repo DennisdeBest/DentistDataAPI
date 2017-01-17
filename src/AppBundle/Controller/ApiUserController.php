f9391700210714ebad1f25982cd2e25328d8cf54
DennisdeBest <dennis@debest.fr>
2017-01-17 14:39:53 +0100

    setRoles array

--- src/AppBundle/Controller/ApiUserController.php
+++ src/AppBundle/Controller/ApiUserController.php
@@ -35,7 +35,7 @@ class ApiUserController extends BaseController
                 $em = $this->getDoctrine()->getEntityManager();
                 $userToPromote = $em->getRepository('AppBundle:User')
                     ->find($id);
-                $userToPromote->setRoles("ROLE_USER");
+                $userToPromote->setRoles(["ROLE_USER"]);
                 $em->flush();
 
                 $response = new Response($this->serialize("User promoted"), Response::HTTP_OK);
