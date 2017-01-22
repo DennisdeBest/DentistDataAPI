e5962788a8ad264a8016dd397ccef2e2b62ed27e
DennisdeBest <dennis@debest.fr>
2017-01-22 22:38:52 +0100

    put back switch case

--- src/AppBundle/Controller/ApiUserController.php
+++ src/AppBundle/Controller/ApiUserController.php
@@ -97,8 +97,39 @@ class ApiUserController extends BaseController
         $logger->info($data);
         $numbers = [];
 
-
-
+        $allForms = new AllForms();
+        //$allForms->setUser($this->getUser()->id);
+        //$logger->info($allForms->getUser());
+        foreach ($data as $key=>$value) {
+            $logger->info($value);
+            if (preg_match_all('/\d+/', $key, $matches)) {
+                $number = $matches[0][0];
+                $logger->info($number);
+                $newNumber = 0;
+                if(!in_array($number, $numbers)){
+                    $newNumber = $number;
+                    $numbers[]= $number;
+                    $entity = "Form".$number;
+                    $logger->info($entity);
+                    //$entityObjects = new $entity();
+                    switch ($number){
+                        case "0":
+                            $entity = new Form0();
+                            break;
+                        case "1":
+                            $entity = new Form1();
+                            break;
+                        default:
+                            $entity = new Form0();
+                    }
+                    $form1 = new Form1();
+                }
+                while($newNumber == $number){
+                    $logger->info($number." ".$key." ".$value);
+                    //$entity->get.$key->$value;
+                }
+            }
+        }
     }
 
     public function jsonToArray($json){
